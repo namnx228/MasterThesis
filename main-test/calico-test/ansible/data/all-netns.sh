86e50b749f78a113f2ff11f6fdb01a7e8d18c214
nam <namnx228@gmail.com>
2020-03-31 01:26:36 +0300

    me nha no

--- main-test/calico-test/ansible/data/all-netns.sh
+++ main-test/calico-test/ansible/data/all-netns.sh
@@ -1,13 +1,18 @@
-list_netns=$(sudo ip netns list | grep default -v | awk '{print $1}' )
+if [ ! -L  /var/run/netns ]
+then
+ sudo ln -s /var/run/docker/netns  /var/run/netns 
+fi
+
+list_netns=$(sudo ip netns list | grep default -v |  awk '{print $1}'  )
 for i in ${list_netns}
 do
- sudo ip  netns  exec $i  iptables -D INPUT 1
- sudo ip  netns  exec $i  iptables -D INPUT 1
- sudo ip  netns  exec $i  iptables -D OUTPUT 1
- sudo ip  netns  exec $i  iptables -D OUTPUT 1
- sudo ip  netns  exec $i  iptables -A INPUT -s 10.0.0.0/28 -d 10.0.0.0/28 -j ACCEPT
- sudo ip  netns  exec $i  iptables -A OUTPUT -s 10.0.0.0/28 -d 10.0.0.0/28 -j ACCEPT
- sudo ip  netns  exec $i  iptables -A INPUT  -s 10.0.0.0/16 -d 10.0.0.0/16 -j DROP
- sudo ip  netns  exec $i  iptables -A OUTPUT  -s 10.0.0.0/16 -d 10.0.0.0/16 -j DROP
+  sudo ip  netns  exec $i iptables -D INPUT 1
+  sudo ip  netns  exec $i iptables -D INPUT 1
+  sudo ip  netns  exec $i iptables -D OUTPUT 1
+  sudo ip  netns  exec $i iptables -D OUTPUT 1
+  sudo ip  netns  exec $i iptables -A INPUT -s 172.16.0.0/28 -d 172.16.0.0/28 -j ACCEPT
+  sudo ip  netns  exec $i iptables -A OUTPUT -s 172.16.0.0/28 -d 172.16.0.0/28 -j ACCEPT
+  sudo ip  netns  exec $i iptables -A INPUT  -s 172.16.0.0/16 -d 172.16.0.0/16 -j DROP
+  sudo ip  netns  exec $i iptables -A OUTPUT  -s 172.16.0.0/16 -d 172.16.0.0/16 -j DROP
 done
 
