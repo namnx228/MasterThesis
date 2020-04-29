bbddcd8006dbd9eba9db77efa7f6e77261a6867d
nam <namnx228@gmail.com>
2020-04-29 18:13:06 +0300

    In process of designing multi tenants evaluatation test

---
+++ main-test/calico-test/multi-tenancy-generator/tenant-generator.sh
@@ -0,0 +1,40 @@
+#! /usr/bin/env bash
+# set -e
+set -x
+# Get list of file 
+# Xu ly tung file:
+# Get cai dir cua no
+# Thay the cai {i} = $i
+# Tao 1 file moi voi ten filename-$i.yml and print new content to.
+
+NUMOFTENANTS=2
+filelist=" \
+  ../namespace/namespace.format \
+  ../role/role-binding.format \
+  ../role/role.format \
+  ../pool/pools.format \
+"
+for file in ${filelist}
+do
+  
+  # directory=$(dirname ${file})
+  fileWithoutExtension="${file%.*}"
+  # echo $file
+  # echo $directory
+  # pushd .
+  # cd ${directory}
+  for i in $(seq 1 ${NUMOFTENANTS})
+  do
+    # cau lenh thay the + sinh file 
+    sed -e "s|\${i}|${i}|g" ${file} > ${fileWithoutExtension}-${i}.yml
+  done
+
+  # popd
+done
+
+#------------------------------------
+# Only Generate one file
+file="../sidecar/perf-sidecar-injector/deployment/configure.format"
+fileWithoutExtension="${file%.*}"
+sed -e "s|\${i}|${NUMOFTENANTS}|g" ${file} > ${fileWithoutExtension}.yml
+
--- main-test/calico-test/run-test-from-sratch.sh
+++ main-test/calico-test/run-test-from-sratch.sh
@@ -1,18 +1,17 @@
 set -ex
-calicoctl apply -f pools.yml
-kubectl apply -f namespace.yml
+calicoctl apply -f pool/
+kubectl apply -f namespace/
+NUMOFTENANT=$(($(ls namespace/ | wc -l) - 1))
+# echo $NUMOFTENANT
 pushd .
 cd user
-./user_creation_flags.sh -u test1 -g O -d 500
-./user_creation_flags.sh -u test2 -g O -d 500
+for i in $(seq 1 ${NUMOFTENANT} )
+do
+  ./user_creation_flags.sh -u "test$i" -g O -d 500
+  kubectl annotate ns "test$i" "cni.projectcalico.org/ipv4pools"='["pool${i}"]'  --overwrite
+done
 popd
-kubectl annotate ns test1 "cni.projectcalico.org/ipv4pools"='["pool1"]'  --overwrite
-kubectl annotate ns test2 "cni.projectcalico.org/ipv4pools"='["pool2"]'  --overwrite
-kubectl apply -f role_ns1.yml
-kubectl apply -f role_ns2.yml
-kubectl apply -f role-binding-ns1.yml
-kubectl apply -f role-binding-ns2.yml
 
-# calicoctl apply -f isolation-vlan1.yml
+kubectl apply -f role/
 
 
