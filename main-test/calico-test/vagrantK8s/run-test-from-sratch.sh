bd0b1459d0408b7f619e0e1e909f9fd6eeb4ba32
nam <namnx228@gmail.com>
2020-04-30 16:44:43 +0300

    Update run-... in vagrantK8s

--- main-test/calico-test/vagrantK8s/run-test-from-sratch.sh
+++ main-test/calico-test/vagrantK8s/run-test-from-sratch.sh
@@ -1,21 +1,19 @@
 set -ex
-kubectl apply -f ../namespace.yml
-pushd .
-cd user
-./user_creation_flags.sh -u test1 -g O -d 500
-./user_creation_flags.sh -u test2 -g O -d 500
-popd
+NUMOFTENANT=$(($(ls namespace/ | wc -l) - 1))
+
+for i in $(seq 1 $NUMOFTENANT)
+do
+  calicoctl apply -f ../pool/pools-${i}.yml | true
+done
+kubectl apply -f ../namespace/
+
+for i in $(seq 1 ${NUMOFTENANT} )
+do
+  ./user/user_creation_flags.sh -u "test$i" -g O -d 500
+  kubectl annotate ns "test$i" "cni.projectcalico.org/ipv4pools"='["pool${i}"]'  --overwrite
+done
 # Assign IP ranges
 # calicoctl delete ippool default-ipv4-ippool # Delete the default ippool, so it is possible to create and allocate IPs manually
-calicoctl apply -f ../pools.yml
-kubectl annotate ns test1 "cni.projectcalico.org/ipv4pools"='["pool1"]'  --overwrite
-kubectl annotate ns test2 "cni.projectcalico.org/ipv4pools"='["pool2"]'  --overwrite
 
-cd ..
-kubectl apply -f role_ns1.yml
-kubectl apply -f role_ns2.yml
-kubectl apply -f role-binding-ns1.yml
-kubectl apply -f role-binding-ns2.yml
 
-# calicoctl apply -f isolation-vlan1.yml
-
+kubectl apply -f ../role/
