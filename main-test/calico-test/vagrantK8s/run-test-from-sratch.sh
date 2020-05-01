ab2cc7a61ed93211a5b9f0805ed6d572f85a0e1a
nam <namnx228@gmail.com>
2020-05-01 13:48:06 +0300

    OR is not pipe

--- main-test/calico-test/vagrantK8s/run-test-from-sratch.sh
+++ main-test/calico-test/vagrantK8s/run-test-from-sratch.sh
@@ -3,32 +3,32 @@ set -ex
 NUMOFTENANT=$(($(ls ../namespace/ | wc -l) - 1))
 MAXNUMOFTENANTS=100
 
-EXISTNS=$(kubectl get ns | grep "test" -c) | true
+EXISTNS=$(kubectl get ns | grep "test" -c) || true
+echo
 if (( $EXISTNS > 0 ))
 then
   for i in  $(seq 1 $EXISTNS)
   do
-    kubectl delete ns test${i} | true
-    kubectl delete role test${i} -n test${i} | true
+    kubectl delete ns test${i} || true
     # kubectl delete -f ../role/role-binding-${i}.yml | true
   done
 fi
 
-EXISTPOOL=$(calicoctl get ippool | grep pool -c) | true
+EXISTPOOL=$(calicoctl get ippool | grep pool -c) || true
 if (( $EXISTPOOL > 0 ))
 then
   for i in  $(seq 1 $EXISTPOOL)
   do
-    calicoctl delete  ippool pool${i}| true
+    calicoctl delete  ippool pool${i} || true
   done
 fi
 
-EXISTROLE=$(kubectl get role --all-namespaces | grep "test" -c) | true
+EXISTROLE=$(kubectl get role --all-namespaces | grep "test" -c) || true
 if (( $EXISTROLE > 0 ))
 then
   for i in  $(seq 1 $EXISTROLE)
   do
-    kubectl delete role test${i} -n test${i} | true
+    kubectl delete role test${i} -n test${i} || true
   done
 fi
 
@@ -41,7 +41,7 @@ cd user/
 for i in $(seq 1 ${NUMOFTENANT} )
 do
   ./user_creation_flags.sh -u "test$i" -g O -d 500
-  kubectl annotate ns "test$i" "cni.projectcalico.org/ipv4pools"='["pool${i}"]'  --overwrite  | true
+  kubectl annotate ns "test$i" "cni.projectcalico.org/ipv4pools"='["pool${i}"]'  --overwrite  || true
 done
 popd
 # Assign IP ranges
