86db93a4745c31ec67ece8befe7db9f17ea02c6f
nam <namnx228@gmail.com>
2020-05-02 13:57:43 +0300

    1zX

--- main-test/calico-test/multi-tenancy-generator/all-in-one-deployment.sh
+++ main-test/calico-test/multi-tenancy-generator/all-in-one-deployment.sh
@@ -5,7 +5,7 @@ then
   ./tenant-generator.sh $1
   yes Y | ./interact-with-k8s.sh
   cd ../sidecar/perf-sidecar-injector/sidecar-container/
-  make release
+  make release || true
 else
   echo "Require one paras: Number of tenants"
 fi
