4e18250158343c48f5586cc1b6e34e5e16fae6b5
nam <namnx228@gmail.com>
2020-05-13 23:05:49 +0300

    cGX

--- main-test/calico-test/evaluation/performance/exp1/startingTime.sh
+++ main-test/calico-test/evaluation/performance/exp1/startingTime.sh
@@ -3,32 +3,9 @@ set -ex
 shopt -s expand_aliases
 alias kubectl="kubectl -n ${USER}"
 DEPLOYMENT_NAME="experienment1"
-REPLICAS=${1:-2} # default number of pods is 2
-kubectl delete deployment ${DEPLOYMENT_NAME} || true
-cat <<SHELL | kubectl apply -f -
-apiVersion: apps/v1
-kind: Deployment
-metadata:
-  name: ${DEPLOYMENT_NAME}
-  labels:
-    app: nginx
-spec:
-  replicas: ${REPLICAS}
-  selector:
-    matchLabels:
-      app: nginx
-  template:
-    metadata:
-      labels:
-        app: nginx
-    spec:
-      containers:
-      - name: nginx
-        image: nginx:1.14.2
-        ports:
-        - containerPort: 80
-SHELL
+REPLICAS_PARAS=${1:-2} # default number of pods is 2
 
+#----------Functions---------------------
 checkDeploymentAvailable (){
   kubectl get deployments.apps ${DEPLOYMENT_NAME} -o jsonpath="{.status.replicas}"
 }
@@ -44,4 +21,49 @@ loopUntilFoundTime(){
   done
 }
 
-time loopUntilFoundTime
+#---------------------------------------
+# FUnction: input: number of pods
+runTestOneTime() {
+  REPLICAS=${1:-2} # default number of pods is 2
+  kubectl delete deployment ${DEPLOYMENT_NAME} || true
+  cat <<SHELL | kubectl apply -f -
+  apiVersion: apps/v1
+  kind: Deployment
+  metadata:
+    name: ${DEPLOYMENT_NAME}
+    labels:
+      app: nginx
+  spec:
+    replicas: ${REPLICAS}
+    selector:
+      matchLabels:
+        app: nginx
+    template:
+      metadata:
+        labels:
+          app: nginx
+      spec:
+        containers:
+        - name: nginx
+          image: nginx:1.14.2
+          ports:
+          - containerPort: 80
+SHELL
+  time loopUntilFoundTime
+}
+
+run30Time(){
+  let sum=0
+  let thisTime=0
+  for i in $(seq 1 30)
+  do
+  # use bash only
+    # runTestOneTime ${REPLICAS_PARAS}
+    thisTime=$(TIMEFORMAT=%R runTestOneTime ${REPLICAS_PARAS})
+    sum=$(python -c "print ${sum} + ${thisTime}")
+  done
+  result=$(python -c "print ${sum} / 30.0")
+  echo ${result}
+}
+
+run30Time
