93ea1c7f65661b611e4f647774fded6157d31081
nam <namnx228@gmail.com>
2020-05-14 13:45:42 +0300

    If test1 --> insecure

--- main-test/calico-test/evaluation/performance/exp1/exp1.sh
+++ main-test/calico-test/evaluation/performance/exp1/exp1.sh
@@ -1,7 +1,13 @@
 #!/usr/bin/env bash
 REPLICAS_LIST=(2 5 10 15 20 25 30 35 40)
 today=$(date +%d.%m-%T)
-result_file="./result/${today}"
+if [[ ${USER} == "test1"  ]]
+then
+  secure="insecure"
+else
+  secure="secure"
+fi
+result_file="./result/${secure}-${today}"
 for i in ${REPLICAS_LIST[@]}
 do
   echo $i >> ${result_file} # Number of replicas
