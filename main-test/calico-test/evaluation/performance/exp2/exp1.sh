b74bc7fa3f1c8de197605d1351bd857b3a2206e4
nam <namnx228@gmail.com>
2020-05-14 22:44:42 +0300

    change number of tenants and command to delete all namespaces

---
+++ main-test/calico-test/evaluation/performance/exp2/exp1.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+REPLICAS_LIST=(2 5 10 15 20 25 30 35 40)
+today=$(date +%d.%m-%T)
+user=${1:-"test1"}
+kubectl label namespace test1 sidecar-injector=disabled --overwrite # Ensure that test1 is in insecure situation 
+if [[ ${user} == "test1"  ]]
+then
+  secure="insecure"
+else
+  secure="secure"
+fi
+result_file="./result/${secure}-${today}"
+for i in ${REPLICAS_LIST[@]}
+do
+  echo $i >> ${result_file} # Number of replicas
+  sudo -u ${user} -H ./run30Times.sh $i >> ${result_file}
+  # printf "\0\0" >> ${result_file}
+  echo -e "------------------------------------\n">> ${result_file}
+done
