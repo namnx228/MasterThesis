59c7249de429e0a4d90f3f217840e8a2841a8fc8
nam <namnx228@gmail.com>
2020-05-29 22:45:14 +0300

    s5g

--- main-test/calico-test/evaluation/performance/exp9/exp1.sh
+++ main-test/calico-test/evaluation/performance/exp9/exp1.sh
@@ -28,7 +28,7 @@ mkdir ./result/${secure} -p
 for i in ${REPLICAS_LIST[@]}
 do
   # echo $i >> ${result_file} # Number of replicas
-  sudo -u ${user} -H ./run30Times.sh $i >> ${result_file}
+  sudo -u ${user} -H ./run30Times.sh $i > ${result_file}
   # printf "\0\0" >> ${result_file}
   # echo -e "------------------------------------\n">> ${result_file}
 done
